6843f841edf07e3e8c234fff4e9f0c3d6a211ceb
NicolasDiel <71090440+NicolasDiel@users.noreply.github.com>
2020-10-04 22:36:12 -0300

    Add files via upload

---
+++ Excersie Managment/MainProgram.java
@@ -0,0 +1,17 @@
+
+public class MainProgram {
+    
+    private ExerciseManagement management;
+    
+        
+
+    public MainProgram(ExerciseManagement management) {
+        this.management = management;
+    }   
+    
+
+    // update here your exercise progress
+    public static int partsCompleted() {
+        return 120;
+    }
+}
